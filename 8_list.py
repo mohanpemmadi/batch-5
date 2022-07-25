8114887b911084dd1e4c397dac9922b3ba691d95
pitchaiah554 <pitchaiah.python@gmail.com>
2022-07-25 07:50:14 +0530

    lists

---
+++ 8_list.py
@@ -0,0 +1,101 @@
+''' List is a collection of ordered elements and it's a mutable '''
+
+''' crud 
+
+create
+read
+update
+delete
+
+'''
+
+# lst = []
+
+# lst = list((1,2,3))
+
+# lst = [100,2000,'abc',12.34,100,'sai']
+
+# print(lst[1])
+# print(lst[-1])
+# print(lst[2:4])
+# print(lst[:4])
+# print(lst[1:])
+# print(lst[1:6:2])
+
+# lst[0] = 123
+
+# lst[0:2] = ['ravi','venky']
+# print(lst)
+
+# lst.append(9000)
+
+# print(lst)
+
+# print(dir(lst))
+
+# append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort'
+
+lst = [100, 900, 100, 200, 100, 300.333, 50]
+print(lst)
+''' append '''
+# lst.append(700)
+# lst.append(800.88)
+# lst.append(["A",'B'])
+
+''' insert '''
+# lst.insert(4,888)
+
+''' extend '''
+
+# lst.extend(['A','B'])
+
+''' index '''
+
+# print(lst.index(100))
+
+''' count '''
+
+# print(lst.count(100))
+# print(lst.count(900))
+# print(lst.count(123))
+
+''' remove '''
+
+# lst.remove(200)
+
+''' pop '''
+
+# lst.pop()   # removes last value and it takes last index as a default index
+
+# lst.pop(1)
+# lst.pop(0)
+
+''' sort '''
+
+# lst.sort()
+
+# lst1 = ['b','a','z','k']
+# lst1.sort()
+# print(lst1)
+
+# print(lst)
+
+''' reverse '''
+# lst.reverse()
+
+''' copy '''
+# lst2 = lst.copy()
+# lst2 = lst
+# print('lst2 ',lst2)
+
+
+''' clear '''
+
+lst.clear()
+
+print('lst - ',lst)
+
+
+
+
+
