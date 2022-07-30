0ff6f3f18686123c300293b26620f2dd5a20e8fe
pitchaiah554 <pitchaiah.python@gmail.com>
2022-07-30 08:06:25 +0530

    if statements

---
+++ 11_if_statement.py
@@ -0,0 +1,91 @@
+'''
+Syntax
+
+if (condition):
+    logic
+
+'''
+''' if '''
+# value  = 100
+# # print(value > 105)
+# if value > 95:  # 100 > 95
+#     print('value is greater')
+#
+# if value < 95:  # 100 < 95
+#     print('95 is greater')
+
+
+# name = input("enter a name")
+#
+# if name == 'sai':  # sai === sai
+#     print('name is sai')
+#
+# if name == 'ravi':
+#     print('name is ravi')
+
+# st = 'qython'
+#
+# if 'p' in st:
+#     print('p exits')
+# if 'q' in st:
+#     print('q exits')
+
+# name = 'sai'
+#
+# if 'sai' is name:
+#     print('1234')
+
+''' if else '''
+
+# value = 50
+#
+# if value > 90:  # 50> 90
+#     print('if executed')
+# else:
+#     print('else executed')
+
+''' if true -> if block
+    if false -> else block '''
+
+# name = input('enter a name: ')
+#
+# print(name)
+#
+# if name == 'sai':
+#     print('one')
+# if name == 'venky':
+#     print('two')
+# if name == 'ravi':
+#     print('three')
+# if name == 'venky':
+#     print('four')
+
+
+''' if elif else '''
+
+name = input('enter a name: ')
+
+print('original name:', name)
+
+if name == 'sai':
+    print('one')
+elif name == 'venky':
+    print('two')
+elif name == 'ravi':
+    print('three')
+elif name == 'mohan':
+    print('four')
+else:
+    print('five')
+
+
+
+
+
+
+
+
+
+
+
+
