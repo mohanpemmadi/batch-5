b3be5656ea4de14e104df012203db0da33d5af59
pitchaiah554 <pitchaiah.python@gmail.com>
2022-08-01 07:37:01 +0530

    for, break and continue

---
+++ 12_for_loop.py
@@ -0,0 +1,48 @@
+
+
+'''  range '''
+
+# print(list(range(10)))
+# for i in range(10): # range(start,end,step)
+#     print(i)
+
+
+# for xyz in range(1,13,2):
+#     print(xyz)
+
+# 1 in [1,2,3]
+
+''' string '''
+
+# name = 'python'
+#
+# for z in 'mohan':
+#     print(z)
+
+
+''' list '''
+
+# lst = [100,200,300,400,500]
+#
+# for value in lst:
+#     print(value)
+#     print(value+10)
+#     print('@@@@@@@@@@@')
+
+''' tuple '''
+
+# tpl = ('sai','venky','mohan','xyz')
+#
+# for name in tpl:
+#     print(name)
+#     print('****************')
+
+''' dictionary '''
+
+dt = {'name':'krishna','age':30,123:'abc',12.34:[1,2,3]}
+
+# print(dt['name'])
+# for i in dt:
+#     print(i)
+#     print(dt[i])
+#     print('!!!!!!!!!!!!!!!')
---
+++ 13_break_continue.py
@@ -0,0 +1,21 @@
+
+''' break - to break the loop on a given condition'''
+
+# for i in range(1,11):
+#     print(i)
+#     if i == 5:
+#         break
+
+# for value in [100,200,300,400,500]:
+#     print(value)
+#     if value == 300:
+#         break
+
+''' continue - used to skip only specific condition '''
+
+for i in range(1,11):
+    if i == 5:
+        continue
+    else:
+        print(i)
+
