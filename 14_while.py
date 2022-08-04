b3c549e1b95979ad61995cccd1f3fd5d43ccf390
pitchaiah554 <pitchaiah.python@gmail.com>
2022-08-04 07:51:39 +0530

    while and programs

---
+++ 14_while.py
@@ -0,0 +1,42 @@
+
+''' syntax '''
+
+"""
+value assignment
+while condition:
+    logic
+    increment
+"""
+
+'''
+
+value = 5
+
+while value <= 10:
+    print(value)
+    value = value+1
+
+
+'''
+
+'''
+value = 10
+
+while value > 5:
+    print(value)
+    value = value +1
+    if value == 20:
+        break
+        
+'''
+
+# lst = []
+x = 1
+while x<=10:
+    print(x, end=' ')
+    x = x +1
+    # print(x)
+
+
+
+
---
+++ programs.py
@@ -0,0 +1,74 @@
+''' even or odd numbers '''
+
+# even = list(range(0,100,2))
+# odd = list(range(1,100,2))
+
+# print('even numbers :',even)
+# print('odd numbers :',odd)
+
+# 15/5  / -> 3       % -> 0
+# for num in range(11):
+#     if num % 2 == 0:
+#         print(num)
+
+
+''' sum of given numbers '''
+
+# lst = [10,20,30,40,50]
+
+'''
+result = 0
+for value in lst:
+    result = result + value
+print(result)
+    
+'''
+
+'''
+for value in lst:
+    result = 0
+    result = result + value
+    print(result)
+'''
+
+''' updating elements in the inner list or tuple '''
+
+
+lst = [100,200,(300,400)]
+# print(lst)
+# lst[0]= 111
+# print(lst)
+# print(lst[2])
+# print(lst[2][0])
+# print(lst[2][1])
+
+# lst[2][0] = 333
+# print(lst)
+
+# lst[2] = 12345
+# print(lst)
+
+tple = (600,700,[800,900])
+# print(tple)
+# tple[0] = 666
+# print(tple)
+
+# print(tple[2])
+#
+# tple[2] = 54321
+
+print(tple[2][0])
+print(tple[2][1])
+
+tple[2][0] = 888
+
+print(tple)
+
+
+
+
+
+
+
+
+
