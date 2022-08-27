ef96fbb9663988d32e3ff9d9753e5ec351b387f5
pitchaiah554 <pitchaiah.python@gmail.com>
2022-08-27 07:42:30 +0530

    database installation

--- 30_database.py
+++ 30_database.py
@@ -5,4 +5,35 @@ install progresql server:
 
 	https://www.enterprisedb.com/downloads/postgres-postgresql-downloads
 
+guide :
+
+	https://www.guru99.com/download-install-postgresql.html
+
+
+install psycopg2:
+
+	pip install psycopg2
+
+'''
+
+'''
+
+database server : batch5
+
+Host name : 127.0.0.1/localhost
+
+database: company
+
+user: postgres
+
+password: root
+
+port: 5432
+
+'''
+
+'''
+
+server -> databases(1 or more) -> tables(1 or more)
+
 '''
